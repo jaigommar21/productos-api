effc72f8444e8887ccd6c6dab07c1a8b5e6f98ea
Jaime <jgomezm@tecsup.edu.pe>
2021-05-13 16:12:21 -0500

    Support token

---
+++ src/main/java/pe/edu/tecsup/productosapi/config/SpringSecurityConfig.java
@@ -0,0 +1,34 @@
+package pe.edu.tecsup.productosapi.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import pe.edu.tecsup.productosapi.filters.JwtAuthenticationTokenFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+	        .authorizeRequests()
+	        .antMatchers("/" /*, "/**"*/).permitAll()
+	        .antMatchers("/auth/login").permitAll()
+	        .and().csrf().disable();
+        
+        http.addFilterBefore(authenticationTokenFilterBean(), 
+        		UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public JwtAuthenticationTokenFilter authenticationTokenFilterBean() throws Exception {
+        return new JwtAuthenticationTokenFilter("/api/**");
+    }
+
+}
--- src/main/java/pe/edu/tecsup/productosapi/controllers/ProductoController.java
+++ src/main/java/pe/edu/tecsup/productosapi/controllers/ProductoController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,6 +26,7 @@ import pe.edu.tecsup.productosapi.entities.Producto;
 import pe.edu.tecsup.productosapi.services.ProductoService;
 
 @RestController
+@RequestMapping("/api")
 public class ProductoController {
 
 	private static final Logger logger = LoggerFactory.getLogger(ProductoController.class);
